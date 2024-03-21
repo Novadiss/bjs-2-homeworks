41630d96621c816a3a1d4d753ec765eb188147a0
Novadis <dark.novadis@gmail.com>
2024-03-21 22:38:35 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let filtered = users.filter(user => user.gender === gender);
+    let result = filtered.map(user => user.age).reduce((acc, curr) => acc + curr, 0);
+    if (result === 0){
+      return 0;
+    } else {
+      return result / filtered.length;
+    }
 }
