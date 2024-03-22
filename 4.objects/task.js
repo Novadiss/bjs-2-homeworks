4ccd23f560dbf83d135609a76926977d51cea3ad
Novadis <dark.novadis@gmail.com>
2024-03-22 20:17:03 +0300

    Четвертое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+	this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks === undefined) {
+        return 0;
+	} else {
+		this.marks.push(...marks);
+	}
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks === undefined){
+        return 0;
+    } else {
+        return this.marks.reduce((acc, item, index, arr) => acc + item / arr.length, 0)
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
 }
